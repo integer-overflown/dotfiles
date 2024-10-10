73af12d44fbb4672197af997d66008ac7888b700
Maksym Khomenko <maksym.khomenko@proton.me>
2024-10-10 22:19:14 +0300

    Add fp keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,11 @@
 local builtin = require('telescope.builtin')
+
+-- Telescope keymaps
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+
+-- Utility keymaps
+vim.keymap.set('n', '<leader>fp', function()
+  vim.cmd "let @+=@%"
+end, { desc = 'Copy current file path to the system clipboard' })
 
