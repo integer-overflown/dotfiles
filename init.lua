ef106021c0effcac908a970b37c8138523559530
Maksym Khomenko <maksym.khomenko@proton.me>
2024-10-13 01:18:35 +0300

    Move options to a separate module

--- init.lua
+++ init.lua
@@ -1,14 +1,5 @@
--- Set the tab width to 4
-vim.opt.shiftwidth = 4
--- Enable line numbers by default
-vim.opt.number = true
--- Enable syntax highlighting
-vim.opt.syntax = "on"
--- Expand tabs to spaces
-vim.opt.expandtab = true
-
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- Configure global vim options
+require("config.options")
 
 -- Load Lazy and the plugins
 require("config.lazy")
