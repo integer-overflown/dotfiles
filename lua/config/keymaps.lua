4d60003532a34effea795581c24a464c0a687049
Maksym Khomenko <maksym.khomenko@proton.me>
2024-10-13 12:59:23 +0300

    Add LSP setup and keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,7 +12,23 @@ end, { desc = 'Copy current file path to the system clipboard' })
 vim.keymap.set('n', '<leader>m', 'o<ESC>', { desc = 'Add a new line under the cursor' })
 vim.keymap.set('n', '<leader>M', 'O<ESC>', { desc = 'Add a new line above the cursor' })
 
+-- Neo-tree keymaps
 vim.keymap.set('n', '<leader>b', ':Neotree toggle show buffers right<cr>', { desc = 'Show currently opened buffers' })
 vim.keymap.set('n', '<leader>s', ':Neotree float git_status<cr>', { desc = 'Open git status in a floating window' })
 vim.keymap.set('n', '|', ':Neotree reveal<cr>', { desc = 'Open neo-tree file view' })
+
+-- LSP keymaps
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('i', '<c-p>', vim.lsp.buf.signature_help, opts)
+  end,
+})
 
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "lua_ls", "clangd", "rust_analyzer" }
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      -- setup lua_ls (Lua LSP)
+      require('lspconfig').lua_ls.setup{}
+
+      -- setup clangd (C/C++/ObjC/ObjC++ support)
+      require('lspconfig').clangd.setup{}
+
+      -- setup rust_analyzer (Rust LSP)
+      require'lspconfig'.rust_analyzer.setup{}
+    end
+  },
+}
+
