0a216332e83f33f3380d0c40ff8b3c651bdb1c72
Maksym Khomenko <maksym.khomenko@proton.me>
2024-10-13 13:13:08 +0300

    Add telescope-ui-select plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,12 @@
 -- install telescope plugin
-return { 
-  "nvim-telescope/telescope.nvim", 
-  tag = "0.1.8", 
-  dependencies = { "nvim-lua/plenary.nvim" } 
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local telescope = require("telescope")
+    telescope.setup({})
+    telescope.load_extension("ui-select")
+  end
 }
 
