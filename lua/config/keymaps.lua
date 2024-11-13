635dda9dde75eb3862188aa41538006ecef3c2b9
Maksym Khomenko <maksym.khomenko@proton.me>
2024-11-13 03:05:53 +0200

    Add LSP 'rename' keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -23,9 +23,17 @@ vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
   callback = function(ev)
     local opts = { buffer = ev.buf }
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.type_definition, opts)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+
+    if client.supports_method("textDocument/rename") then
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    end
+
+    if client.supports_method("textDocument/implementation") then
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    end
+
     vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
     vim.keymap.set("n", "gr", function()
