9ff37e8fa5c96cbbd56fdb97c496fe33f28a3f8f
Maksym Khomenko <maksym.khomenko@proton.me>
2024-11-24 00:28:16 +0200

    treesitter: add JSON to ensure_installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "c", "lua", "rust", "markdown", "cpp" },
+      ensure_installed = { "c", "lua", "rust", "markdown", "cpp", "json" },
       highlight = { enable = true },
       indent = { enable = true },
     })
