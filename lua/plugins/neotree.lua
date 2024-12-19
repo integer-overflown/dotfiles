b05b716d1d761bf3afb266f0601559400ee0b4cd
Maksym Khomenko <maksym.khomenko@proton.me>
2024-12-19 22:47:24 +0200

    neotree: add navigate_to_parent util

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -29,11 +29,27 @@ return {
   },
   config = true,
   opts = {
+    commands = {
+      ["navigate_to_parent"] = function(state)
+        local node = state.tree:get_node()
+        print(vim.inspect(node))
+
+        local parent_node = node:get_parent_id()
+
+        if parent_node == nil then
+          return
+        end
+
+        local path = state.tree:get_node(parent_node):get_id()
+        require("neo-tree.sources.filesystem").navigate(state, state.path, path)
+      end,
+    },
     window = {
       mappings = {
         ["<cr>"] = "open_with_window_picker",
         ["v"] = "vsplit_with_window_picker",
         ["s"] = "split_with_window_picker",
+        ["K"] = "navigate_to_parent",
       },
     },
   },
