aa65c9337bd455cfe052a3c14eea6cd73a2a9583
Maksym Khomenko <maksym.khomenko@proton.me>
2025-01-02 19:21:11 +0200

    Enable spellcheck for all editable buffers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,9 +16,6 @@ vim.opt.autoread = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Enable spellcheck for English
-vim.cmd("setlocal spell spelllang=en_us")
-
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
@@ -40,5 +37,41 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("ovf-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+local function make_ignore_map(filetypes)
+  local ret = {}
+
+  for _, value in ipairs(filetypes) do
+    ret[value] = true
+  end
+
+  return ret
+end
+
+local ignored = make_ignore_map({
+  "TelescopePrompt",
+  "TelescopePromptBorder",
+  "TelescopeResults",
+  "dashboard",
+  "neo-tree",
+  "neo-tree-popup",
+  "notify",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  desc = "Enable spell-check for editable buffers",
+  group = vim.api.nvim_create_augroup("ovf-buf-spellcheck", { clear = true }),
+  callback = function(args)
+    local ft = args.match
+
+    if ignored[ft] then
+      return
+    end
+
+    local win = vim.api.nvim_get_current_win()
+    vim.wo[win].spell = true
+    vim.bo[args.buf].spelllang = "en_us"
   end,
 })
