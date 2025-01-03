3e121b8ef8b735c6e30a2d69d9c99e0c00726488
Maksym Khomenko <maksym.khomenko@proton.me>
2025-01-03 20:43:19 +0200

    Use transparent background

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,15 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
+    local theme = require("catppuccin")
+
+    local opts = {
+      transparent_background = true,
+      show_end_of_buffer = true,
+    }
+
+    theme.setup(opts)
+
     -- Set the default color scheme
     vim.cmd([[colorscheme catppuccin-macchiato]])
   end,
