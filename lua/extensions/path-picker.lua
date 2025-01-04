5f0df2eb10f823fff4a655cd6afaa0a44d2a473a
Maksym Khomenko <maksym.khomenko@proton.me>
2025-01-04 02:22:24 +0200

    Implement a telescope-based picker for paths

--- lua/extensions/path-picker.lua
+++ lua/extensions/path-picker.lua
@@ -1,56 +1,90 @@
 local options = { "absolute", "relative" }
 
-local function pick_path(type, out_reg)
-  local reg = out_reg or "@"
-
+local function get_path(path, type)
   local pickers = {
     ["absolute"] = function()
-      local val = vim.fn.expand("%:p")
-
-      vim.fn.setreg(reg, val)
-      return val
+      return vim.fn.fnamemodify(path, ":p")
     end,
     ["relative"] = function()
-      local val = vim.fn.expand("%:.")
-
-      vim.fn.setreg(reg, val)
-      return val
+      return vim.fn.fnamemodify(path, ":.")
     end,
   }
 
   local picker = pickers[type]
 
-  if picker == nil then
-    return
+  if picker then
+    return picker()
   end
+end
 
-  local picked = picker()
-
-  if #picked == 0 then
+local function confirm_selection(result, out_reg)
+  if #result == 0 then
     print("No file path")
     return
   end
 
+  local reg = out_reg or "@"
+
+  vim.fn.setreg(reg, result)
+
   if out_reg then
-    print("Picked", picked, "to", out_reg)
+    print("Picked", result, "to", out_reg)
   else
-    print("Picked", picked)
+    print("Picked", result)
   end
 end
 
+local function pick_path(type)
+  local path = vim.fn.getreg("%")
+  local result = get_path(path, type)
+
+  confirm_selection(result)
+end
+
 local function open_path_picker(out_reg)
-  vim.ui.select(options, {
-    prompt = "Select path type",
-  }, function(item)
-    if item ~= nil then
-      pick_path(item, out_reg)
-    end
-  end)
+  local pickers = require("telescope.pickers")
+  local finders = require("telescope.finders")
+  local preview = require("telescope.previewers")
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+
+  -- since the picker is a separate buffer, we need to save and
+  -- recall the actual buffer name later
+  local path = vim.fn.getreg("%")
+  local opts = {}
+
+  pickers
+    .new(
+      opts,
+      require("telescope.themes").get_dropdown({
+        finder = finders.new_table(options),
+        previewer = preview.new_buffer_previewer({
+          define_preview = function(entry, status)
+            local type = status[1]
+            local result = get_path(path, type)
+
+            vim.api.nvim_buf_set_lines(entry.state.bufnr, 0, -1, true, { result })
+          end,
+        }),
+        attach_mappings = function(prompt_bufnr)
+          actions.select_default:replace(function()
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+
+            local type = selection[1]
+            local result = get_path(path, type)
+
+            confirm_selection(result, out_reg)
+          end)
+          return true
+        end,
+      })
+    )
+    :find()
 end
 
 vim.api.nvim_create_user_command("PickPath", function(cmd)
   -- the function is nil-safe, so we're fine if no args were passed
-  vim.print(cmd)
   open_path_picker(cmd.fargs[1])
 end, {
   desc = "Open file path picker",
