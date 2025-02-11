a2396af240140eb63cc64328cc7e576a1ad4feeb
Maksym Khomenko <maksym.khomenko@proton.me>
2025-02-11 04:49:30 +0200

    Support overriding task ID with an env var

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -11,7 +11,15 @@ end
 
 local nio = require("nio")
 
-nio.run(function()
+local function get_task_id()
+  -- Strategy 1: env variable
+  local env_task = os.getenv("TASK")
+
+  if env_task then
+    return env_task
+  end
+
+  -- Strategy 2: parse a task id from a branch name, according to the config
   local process, err = nio.process.run({
     cmd = "git",
     args = { "branch", "--show-current" },
@@ -26,21 +34,37 @@ nio.run(function()
 
   if not process then
     e(err)
+    return
   end
 
   local exit_code = process.result(false)
 
   if exit_code ~= 0 then
     e("non-zero exit code")
+    return
   end
 
   local branch_name = process.stdout.read()
 
+  process.close()
+
   local conf = require("utils.config")
   local task_template = conf.read_field(conf.load_config().wait(), "string", "git", "task_template")
+
+  if not task_template then
+    return
+  end
+
+  return string.match(branch_name, task_template)
+end
+
+nio.run(function()
+  local conf = require("utils.config")
+
+  local task_id = get_task_id()
   local message_template = conf.read_field(conf.load_config().wait(), "string", "git", "message_template")
 
-  if not (task_template and message_template) then
+  if not (task_id and message_template) then
     -- There's no data to auto-fill, but let's save pressing a single letter and
     -- enter insert mode anyway
     vim.schedule(function()
@@ -49,18 +73,10 @@ nio.run(function()
     return
   end
 
-  local task = string.match(branch_name, task_template)
-
-  if not task then
-    return
-  end
-
-  local message = string.gsub(message_template, "%$task", task)
+  local message = string.gsub(message_template, "%$task", task_id)
 
   vim.schedule(function()
     vim.api.nvim_buf_set_lines(0, 0, 1, true, { message })
     vim.cmd("startinsert!")
   end)
-
-  process.close()
 end)
