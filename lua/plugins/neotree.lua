e72cbdcbd1899ce33dee0e864e1733f306fb2c16
Maksym Khomenko <maksym.khomenko@proton.me>
2025-02-27 17:37:37 +0200

    Neotree: add document_symbols, g<cr>, tidying up

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,3 +1,17 @@
+local function navigate_to_parent(state)
+  local node = state.tree:get_node()
+  print(vim.inspect(node))
+
+  local parent_node = node:get_parent_id()
+
+  if parent_node == nil then
+    return
+  end
+
+  local path = state.tree:get_node(parent_node):get_id()
+  require("neo-tree.sources.filesystem").navigate(state, state.path, path)
+end
+
 -- install neo-tree
 return {
   "nvim-neo-tree/neo-tree.nvim",
@@ -9,24 +23,20 @@ return {
   },
   config = true,
   opts = {
+    sources = { "filesystem", "buffers", "git_status", "document_symbols" },
+    document_symbols = {
+      follow_cursor = true,
+    },
+    filesystem = {
+      follow_current_file = true,
+    },
     commands = {
-      ["navigate_to_parent"] = function(state)
-        local node = state.tree:get_node()
-        print(vim.inspect(node))
-
-        local parent_node = node:get_parent_id()
-
-        if parent_node == nil then
-          return
-        end
-
-        local path = state.tree:get_node(parent_node):get_id()
-        require("neo-tree.sources.filesystem").navigate(state, state.path, path)
-      end,
+      ["navigate_to_parent"] = navigate_to_parent,
     },
     window = {
       mappings = {
         ["<cr>"] = "open_with_window_picker",
+        ["g<cr>"] = "open",
         ["v"] = "vsplit_with_window_picker",
         ["s"] = "split_with_window_picker",
         ["K"] = "navigate_to_parent",
