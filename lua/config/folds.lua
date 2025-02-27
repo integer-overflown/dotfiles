4f1df7d639436c610f805d635cf73bb03e309f5d
Maksym Khomenko <maksym.khomenko@proton.me>
2025-02-27 22:37:40 +0200

    folds: different levels based on a pattern

--- lua/config/folds.lua
+++ lua/config/folds.lua
@@ -1,7 +1,32 @@
+local fold_level_by_pattern = {
+  -- include guards create a fold
+  -- since headers always contain them, having a larger
+  -- fold level helps see the contents
+  [".h$"] = 2,
+}
+
+local fold_level_fallback = 1
+
+local function get_buffer_fold_level(buffer)
+  local bufname = vim.api.nvim_buf_get_name(buffer)
+
+  if not bufname then
+    return
+  end
+
+  for pt, level in pairs(fold_level_by_pattern) do
+    if string.match(bufname, pt) then
+      return level
+    end
+  end
+
+  return fold_level_fallback
+end
+
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
   group = vim.api.nvim_create_augroup("ovf-fold-setup", {}),
-  callback = function()
-    vim.opt.foldlevel = 1
+  callback = function(event)
+    vim.opt.foldlevel = get_buffer_fold_level(event.buf)
     vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
     vim.opt.foldmethod = "expr"
   end
