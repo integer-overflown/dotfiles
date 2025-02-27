869e09281706f2801899cd31b91b547ff361d9ea
Maksym Khomenko <maksym.khomenko@proton.me>
2025-02-27 17:59:57 +0200

    ext.reveal: fix a typo

--- lua/extensions/reveal.lua
+++ lua/extensions/reveal.lua
@@ -2,4 +2,4 @@
 
 vim.api.nvim_create_user_command("Reveal", function()
   vim.ui.open(vim.fn.getcwd())
-end, { desc = "Reveal the propath-pickerject directory in a file explorer" })
+end, { desc = "Reveal the project directory in a file explorer" })
